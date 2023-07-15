a53d9537c48ca97e234f05cdea78e07bfc211b6e
zhayt <turk.883ar@gmail.com>
2023-07-15 07:32:20 -0400

    update redis: remove print pong

--- storage/redis/redis.go
+++ storage/redis/redis.go
@@ -5,7 +5,6 @@ import (
 	"fmt"
 	"github.com/redis/go-redis/v9"
 	"github.com/zhayt/cert-tz/config"
-	"log"
 	"net"
 	"time"
 )
@@ -22,12 +21,10 @@ func NewRedisClient(cfg *config.Config) (*redis.Client, error) {
 	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
 	defer cancel()
 
-	pong, err := client.Ping(ctx).Result()
+	_, err := client.Ping(ctx).Result()
 	if err != nil {
 		return nil, fmt.Errorf("cannot connect to redis: %w", err)
 	}
-
-	log.Println(pong)
 
 	return client, nil
 }
