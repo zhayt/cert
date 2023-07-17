e307e2f28bccd0b3c0c77ef361dd8514c56de959
zhayt <turk.883ar@gmail.com>
2023-07-16 22:50:11 -0400

    change certhash model

--- model/hash.go
+++ model/hash.go
@@ -4,7 +4,7 @@ import "time"
 
 type CertHash struct {
 	ID           uint64    `json:"id" db:"id"`
-	InputStr     string    `json:"data" db:"input_str"`
+	InputStr     string    `json:"input_str" db:"input_str" validate:"required,min=1,max=255"`
 	Hash         string    `json:"hash" db:"hash"`
 	CreatedAt    time.Time `json:"created_at" db:"created_at"`
 	CalculatedAt time.Time `json:"calculated_at" db:"calculated_at"`
